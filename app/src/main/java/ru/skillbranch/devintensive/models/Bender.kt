d0a54d4b8e1affb31c5c779a49eef5cb9fe26371
Vlad <0vsyannikovv24@gmail.com>
2020-07-30 00:23:42 +0700

    fixed listenAnswer cases

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -14,11 +14,11 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
         return if (question.answer.contains(answer) || question == Question.IDLE) {
             question = question.nextQuestion()
-            "Отлично − это правильный ответ!\n${question.question}" to status.color
+            "Отлично - ты справился\n${question.question}" to status.color
         } else {
             status = status.nextStatus()
             if (status == Status.NORMAL) {
-                "Это не правильный ответ! Давай всё по новой.\n${question.question}" to status.color
+                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
             } else {
                 "Это не правильный ответ!\n${question.question}" to status.color
             }
@@ -56,7 +56,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
         SERIAL("Мой серийный номер?", listOf("2716057")) {
             override fun nextQuestion(): Question = IDLE
         },
-        IDLE("На этом всё, вопросов больше нет", listOf()) {
+        IDLE("На этом все, вопросов больше нет", listOf()) {
             override fun nextQuestion(): Question = IDLE
         };
 
